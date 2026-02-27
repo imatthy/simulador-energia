15b4699db113b9b374c8a2be820ad298601a1fa9
unknown <Matheus Castro>
2026-02-27 06:23:59 -0300

    Separação da home por categorias de visitantes e melhoras na interface dos formularios.

--- SimuladorEnergiaWASM/Models/PainelSolar.cs
+++ SimuladorEnergiaWASM/Models/PainelSolar.cs
@@ -8,6 +8,8 @@ namespace SimuladorEnergiaWASM.Models
         [Range(1, 100000)]
         public int Potencia { get; set; } = 550;
         public double Eficiencia { get; set; } = 80;
+        [Range(0, double.MaxValue)]
+        public decimal Custo { get; set; } = 1500m;
         [Required]
         [Range(1, 24)]
         public int HorasSol { get; set; } = 5;
